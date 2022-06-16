aa4fad6b9672304ca20afe21e195b9910693d2bb
Deepred <deepred.kim@gmail.com>
2022-06-16 10:37:58 +0900

    fix: OrderRepository findById JPA N+1 이슈 해결

--- src/main/java/kr/flab/momukji/repository/OrderRepository.java
+++ src/main/java/kr/flab/momukji/repository/OrderRepository.java
@@ -2,8 +2,12 @@ package kr.flab.momukji.repository;
 
 import kr.flab.momukji.entity.Order;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    
+    @EntityGraph(attributePaths = {"product_id"})
+    Optional<Order> findById(Long id);
 }
