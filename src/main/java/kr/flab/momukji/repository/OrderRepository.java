7750d1d5240438ea18d43cac63e19bf3a9fd3f53
sue4869 <sue4869@naver.com>
2022-06-19 13:57:14 +0900

    bug : #issue24 해결
    EntityGraph 해결

--- src/main/java/kr/flab/momukji/repository/OrderRepository.java
+++ src/main/java/kr/flab/momukji/repository/OrderRepository.java
@@ -4,10 +4,9 @@ import kr.flab.momukji.entity.Order;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @EntityGraph(attributePaths = {"product_id"})
+ 
     Optional<Order> findById(Long id);
 }
