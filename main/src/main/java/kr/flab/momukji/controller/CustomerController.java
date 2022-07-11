f10fc435adca5f5b8b66d5334ac281dd902bdf03
Deepred <deepred.kim@gmail.com>
2022-07-11 11:45:49 +0900

    refactor: 사용하지 않는 import 삭제

--- main/src/main/java/kr/flab/momukji/controller/CustomerController.java
+++ main/src/main/java/kr/flab/momukji/controller/CustomerController.java
@@ -8,7 +8,6 @@ import kr.flab.momukji.dto.response.common.CommonResponse;
 import kr.flab.momukji.service.OrderService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
