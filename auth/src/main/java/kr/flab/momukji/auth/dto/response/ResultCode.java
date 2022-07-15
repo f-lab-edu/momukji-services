098f275bceec2581e8936944768874806611c94c
sue4869 <sue4869@naver.com>
2022-07-15 11:07:54 +0900

    refator: ResultCode의 토큰결과값 이름 변경

--- auth/src/main/java/kr/flab/momukji/auth/dto/response/ResultCode.java
+++ auth/src/main/java/kr/flab/momukji/auth/dto/response/ResultCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ResultCode {
-    SUCCESS("성공"), VALID("유효한 토큰"), INVALID("잘못된 토큰");
+    SUCCESS("성공"), VALID_TOKEN("유효한 토큰"), INVALID_TOKEN("잘못된 토큰");
 
     private final String message;
 
